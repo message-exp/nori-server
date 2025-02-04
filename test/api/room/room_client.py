aa63650c6c85106e1b77784e7d6b813e902f7afb
ErEr <jeffliao92316046@gmail.com>
2025-02-04 18:59:22 +0000

    Chore: run ruff and mypy

--- test/api/room/room_client.py
+++ test/api/room/room_client.py
@@ -4,17 +4,16 @@ from src.proto_generated.nori.v0.room.room_service_pb2_grpc import RoomServiceSt
 from src.proto_generated.nori.v0.user.user_id_pb2 import UserId
 
 
-
 def create_room() -> None:
     # 建立與 gRPC 伺服器的連接
-    channel = grpc.insecure_channel('localhost:3000')
+    channel = grpc.insecure_channel("localhost:3000")
     stub = RoomServiceStub(channel)
     # 準備要發送的請求資料
     request = RoomCreateRequest(
         user_id=UserId(user_id=7292453983640096768),  # 假設這是用戶 ID
-        name="Test Room"  # 假設這是房間名稱
+        name="Test Room",  # 假設這是房間名稱
     )
-    
+
     try:
         # 呼叫 CreateRoom 方法並接收回應
         response = stub.CreateRoom(request)
@@ -26,5 +25,6 @@ def create_room() -> None:
         # 處理錯誤情況
         print(f"Error creating room: {e.code()} - {e.details()}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_room()
--- test/api/room/room_service_test.py
+++ test/api/room/room_service_test.py
@@ -5,7 +5,7 @@ from typing import Generator, Tuple
 from grpc import ServicerContext
 from pytest_mock import MockerFixture
 
-from src.api.room.room_service import RoomServicer,RoomMembers, Rooms
+from src.api.room.room_service import RoomServicer, RoomMembers, Rooms
 from src.proto_generated.nori.v0.room.room_id_pb2 import RoomId
 from src.proto_generated.nori.v0.user.user_id_pb2 import UserId
 from src.proto_generated.nori.v0.room.room_create_request_pb2 import RoomCreateRequest
@@ -17,13 +17,10 @@ def mock_repositories(
 ) -> Generator[Tuple[MagicMock, MagicMock, MagicMock], None, None]:
     """Mock UserRepository, RoomRepository, RoomMemberRepository"""
     mock_db_session = MagicMock()
-    mocker.patch("src.api.room.room_service.get_db",
-                 return_value=mock_db_session)
+    mocker.patch("src.api.room.room_service.get_db", return_value=mock_db_session)
 
-    mock_user_repo: MagicMock = mocker.patch(
-        "src.api.room.room_service.UserRepository")
-    mock_room_repo: MagicMock = mocker.patch(
-        "src.api.room.room_service.RoomRepository")
+    mock_user_repo: MagicMock = mocker.patch("src.api.room.room_service.UserRepository")
+    mock_room_repo: MagicMock = mocker.patch("src.api.room.room_service.RoomRepository")
     mock_room_member_repo: MagicMock = mocker.patch(
         "src.api.room.room_service.RoomMemberRepository"
     )
@@ -63,9 +60,7 @@ def test_create_room_success(
 
     mock_user_repo.return_value.get_user.assert_called_once_with(user_id=1)
     mock_room_repo.return_value.create_room.assert_called_once_with(
-        Rooms(
-            name="Test Room"
-        )
+        Rooms(name="Test Room")
     )
     mock_room_member_repo.return_value.create_room_member.assert_called_once_with(
         RoomMembers(
@@ -94,5 +89,4 @@ def test_create_room_user_not_found(
     assert response.room_id == 0
 
     grpc_context.set_code.assert_called_once_with(grpc.StatusCode.NOT_FOUND)
-    grpc_context.set_details.assert_called_once_with(
-        "User with ID 999 not found.")
+    grpc_context.set_details.assert_called_once_with("User with ID 999 not found.")
