24400add8712e073ba219503f82a45d5769598e3
barney9211 <barney9211@gmail.com>
2025-02-04 12:19:05 +0000

    Fix:use sqlmodel instead of sql

--- src/repositories/message_repository.py
+++ src/repositories/message_repository.py
@@ -1,26 +1,21 @@
-from sqlmodel import Session
-import psycopg2
-
-conn = psycopg2.connect(host = "localhost" , dbname = "postgres" ,user = "postgres" , password="524162027" , port = 5432)
-cur = conn.cursor()
-
-
-
+from sqlmodel import Session ,select
+from model import Messages
 
 class MessageRepository:
     def __init__(self, session: Session) -> None:
         self.db = session
-    def get_message_by_roomId(self ,room_id) -> dict:
-        cur.execute(f"""SELECT * FROM test WHERE rid = {room_id}""")
-        find_result = cur.fetchone()
-        print(find_result)
-        return {
-            'room_member_id' :find_result[1],
-            'room_id' : find_result[2],
-            'message' : find_result[3],
-            'created_at' : find_result[4],
-            'update_at' : find_result[5],
-        }
+    def get_message_by_roomId(self ,room_id : int) -> Messages:
+        # cur.execute(f"""SELECT * FROM test WHERE rid = {room_id}""")
+        # find_result = cur.fetchone()
+        # print(find_result)
+        # return {
+            # 'room_member_id' :find_result[1],
+            # 'room_id' : find_result[2],
+            # 'message' : find_result[3],
+            # 'created_at' : find_result[4],
+            # 'update_at' : find_result[5],
+        # }
+        return self.db.exec(select(Messages).where(Messages.id == room_id)).first()
 
    
 
