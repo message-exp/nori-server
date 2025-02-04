47bc09b537102c1a61b61dcb6a8b2795a35ff443
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-04 19:08:23 +0800

    feat: add expiration time to generate_token

--- src/utils/token_helper.py
+++ src/utils/token_helper.py
@@ -1,5 +1,7 @@
 import os
 import jwt
+from datetime import datetime
+from typing import Any, Optional
 
 SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default_secret")
 ALGORITHM = "HS512"
@@ -13,5 +15,22 @@ def get_token(token: str | bytes) -> dict:
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 
-def generate_token(user_id: str) -> str:
-    return jwt.encode({"sub": user_id}, SECRET_KEY, algorithm=ALGORITHM)
+def generate_token(
+    subject: Optional[str] = None, expire: Optional[datetime] = None
+) -> str:
+    """
+    Generate a token with the user_id.
+
+    Parameters:
+        subject (str, optional): user_id to be encoded in the token
+        expire (datetime, optional): expiration time of the token
+    """
+    payload: dict[str, Any] = dict()
+
+    if subject is not None:
+        payload["sub"] = subject
+
+    if expire is not None:
+        payload["exp"] = expire
+
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
