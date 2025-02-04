88b10411423ceb32b8b0fd11de2ba31c7c2e3821
ErEr <jeffliao92316046@gmail.com>
2025-02-04 18:30:56 +0000

    Feat: add RoomService to gRPC server and enable server reflection

--- src/api/server.py
+++ src/api/server.py
@@ -1,9 +1,15 @@
 import grpc
 
 from api.user.user_service import UserServicer
+from api.room.room_service import RoomServicer
 from proto_generated.nori.v0.user.user_service_pb2_grpc import (
     add_UserServiceServicer_to_server,
 )
+from proto_generated.nori.v0.room.room_service_pb2_grpc import (
+    add_RoomServiceServicer_to_server,
+)
+from proto_generated.nori.v0.room import room_service_pb2
+from grpc_reflection.v1alpha import reflection
 # from api.room.room_service import RoomServicer
 # from api.message.message_service import MessageServicer
 
@@ -12,6 +18,12 @@ from proto_generated.nori.v0.user.user_service_pb2_grpc import (
 def get_server() -> grpc.Server:
     server = grpc.aio.server()
 
-    add_UserServiceServicer_to_server(servicer=UserServicer, server=server)
+    add_UserServiceServicer_to_server(servicer=UserServicer(), server=server)
+    add_RoomServiceServicer_to_server(servicer=RoomServicer(), server=server)
+    SERVICE_NAMES = (
+        room_service_pb2.DESCRIPTOR.services_by_name["RoomService"].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
     # TODO: add other services
     return server
