2fb92db0646ecd503f75e5eca29cca7b0860255b
ErEr <jeffliao92316046@gmail.com>
2025-02-05 05:58:49 +0000

    Chore: run ruff

--- src/api/server.py
+++ src/api/server.py
@@ -10,15 +10,14 @@ from proto_generated.nori.v0.room.room_service_pb2_grpc import (
     add_RoomServiceServicer_to_server,
 )
 from proto_generated.nori.v0.message.message_service_pb2_grpc import (
-    add_MessageServiceServicer_to_server
+    add_MessageServiceServicer_to_server,
 )
 from proto_generated.nori.v0.room import room_service_pb2
 from grpc_reflection.v1alpha import reflection
 
 
-
-# server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 def get_server() -> grpc.Server:
+    # server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server = grpc.aio.server()
 
     add_UserServiceServicer_to_server(servicer=UserServicer(), server=server)
