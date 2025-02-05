e33402008a29597f882f598c12f9e4bbd1fcd981
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-05 17:04:53 +0800

    fix: ruff format error

--- src/utils/token_context.py
+++ src/utils/token_context.py
@@ -1,13 +1,15 @@
 from contextvars import ContextVar, Token
 
+
 class TokenPayloadHolder:
     def __init__(self) -> None:
         self._var: ContextVar = ContextVar("token_payload")
-    
+
     def set(self, value: dict) -> Token:
         return self._var.set(value)
-    
+
     def get(self) -> dict:
         return self._var.get(None)
+
 
 token_payload = TokenPayloadHolder()
