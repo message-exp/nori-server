c90f33d9b09b480638195b8dd0eded4bdc36bcf3
ErEr <jeffliao92316046@gmail.com>
2025-02-05 05:56:31 +0000

    Feat: add MessageService to gRPC server

--- src/api/server.py
+++ src/api/server.py
@@ -2,16 +2,19 @@ import grpc
 
 from api.user.user_service import UserServicer
 from api.room.room_service import RoomServicer
+from api.message.message_service import MessageServicer
 from proto_generated.nori.v0.user.user_service_pb2_grpc import (
     add_UserServiceServicer_to_server,
 )
 from proto_generated.nori.v0.room.room_service_pb2_grpc import (
     add_RoomServiceServicer_to_server,
 )
+from proto_generated.nori.v0.message.message_service_pb2_grpc import (
+    add_MessageServiceServicer_to_server
+)
 from proto_generated.nori.v0.room import room_service_pb2
 from grpc_reflection.v1alpha import reflection
-# from api.room.room_service import RoomServicer
-# from api.message.message_service import MessageServicer
+
 
 
 # server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -20,6 +23,7 @@ def get_server() -> grpc.Server:
 
     add_UserServiceServicer_to_server(servicer=UserServicer(), server=server)
     add_RoomServiceServicer_to_server(servicer=RoomServicer(), server=server)
+    add_MessageServiceServicer_to_server(servicer=MessageServicer(), server=server)
     SERVICE_NAMES = (
         room_service_pb2.DESCRIPTOR.services_by_name["RoomService"].full_name,
         reflection.SERVICE_NAME,
