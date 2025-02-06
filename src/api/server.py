bb13f2963e6ab4c5a128133e79e045c5af485e35
ErEr <jeffliao92316046@gmail.com>
2025-02-06 07:51:07 +0000

    Feat: add UserService and MessageService to SERVICE_NAMES

--- src/api/server.py
+++ src/api/server.py
@@ -13,7 +13,11 @@ from proto_generated.nori.v0.message.message_service_pb2_grpc import (
     add_MessageServiceServicer_to_server,
 )
 from proto_generated.nori.v0.room import room_service_pb2
+from proto_generated.nori.v0.message import message_service_pb2
+from proto_generated.nori.v0.user import user_service_pb2
 from grpc_reflection.v1alpha import reflection
+
+
 
 
 def get_server() -> grpc.Server:
@@ -24,7 +28,9 @@ def get_server() -> grpc.Server:
     add_RoomServiceServicer_to_server(servicer=RoomServicer(), server=server)
     add_MessageServiceServicer_to_server(servicer=MessageServicer(), server=server)
     SERVICE_NAMES = (
+        user_service_pb2.DESCRIPTOR.services_by_name["UserService"].full_name,
         room_service_pb2.DESCRIPTOR.services_by_name["RoomService"].full_name,
+        message_service_pb2.DESCRIPTOR.services_by_name["MessageService"].full_name,
         reflection.SERVICE_NAME,
     )
     reflection.enable_server_reflection(SERVICE_NAMES, server)
