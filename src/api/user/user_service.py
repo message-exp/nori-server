c4d885cf1dea7d2c3b5b9307dae8b06956c440db
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-08 21:44:54 +0800

    feat: get user room list

--- src/api/user/user_service.py
+++ src/api/user/user_service.py
@@ -1,3 +1,4 @@
+import grpc
 from grpc.aio import ServicerContext
 
 from google.protobuf.empty_pb2 import Empty
@@ -5,11 +6,18 @@ from proto_generated.nori.v0.user.user_pb2 import User
 from proto_generated.nori.v0.user.user_login_pb2 import UserEmailPasswordLogin
 from proto_generated.nori.v0.user.user_id_pb2 import UserId
 from proto_generated.nori.v0.user.user_profile_pb2 import UserProfile
+from proto_generated.nori.v0.room.room_id_pb2 import RoomId
 from proto_generated.nori.v0.room.room_list_pb2 import RoomList
+from proto_generated.nori.v0.room.room_basic_info_response_pb2 import (
+    RoomBasicInfoResponse,
+)
 
 from proto_generated.nori.v0.user.user_service_pb2_grpc import (
     UserServiceServicer,
 )
+
+from utils.db_helper import get_db
+from repositories.room_member_repository import RoomMemberRepository
 
 
 class UserServicer(UserServiceServicer):
@@ -46,5 +54,38 @@ class UserServicer(UserServiceServicer):
         return Empty()
 
     def GetUserRoomList(self, request: UserId, context: ServicerContext) -> RoomList:
-        # TODO: ...... (implement get user room list logic)
-        return RoomList()
+        # get user ID from request
+        user_id = request.id
+
+        # get user room_member data from database
+        room_member_repository = RoomMemberRepository(next(get_db()))
+        user_room_members = room_member_repository.get_user_room_members(user_id)
+
+        # check if user room_member object exists in database
+        if user_room_members is None:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"User with ID {user_id} has no associated rooms.")
+            return RoomList()
+
+        rooms = []
+        for room_member in user_room_members:
+            # For the name oneof, prioritize the custom name if provided.
+            if room_member.room_name:
+                name_field = {"custom_name": room_member.room_name}
+            else:
+                name_field = {"shared_name": room_member.room.name}
+
+            # For the avatar oneof, prioritize the custom URL if provided.
+            if room_member.room_avatar_url:
+                avatar_field = {"custom_avatar_url": room_member.room_avatar_url}
+            else:
+                avatar_field = {"shared_avatar_url": room_member.room.avatar_url}
+
+            room_basic_info = RoomBasicInfoResponse(
+                room_id=RoomId(id=room_member.room_id),
+                **name_field,
+                **avatar_field,
+            )
+            rooms.append(room_basic_info)
+
+        return RoomList(rooms=rooms)
--- src/repositories/room_member_repository.py
+++ src/repositories/room_member_repository.py
@@ -1,10 +1,16 @@
-from sqlmodel import Session
+from sqlmodel import Session, select
 from model import RoomMembers
 
 
 class RoomMemberRepository:
     def __init__(self, session: Session) -> None:
         self.db = session
+
+    def get_user_room_members(self, user_id: int) -> list[RoomMembers] | None:
+        result = self.db.exec(
+            select(RoomMembers).where(RoomMembers.user_id == user_id)
+        ).all()
+        return list(result) if result else None
 
     def create_room_member(self, room_member: RoomMembers) -> int:
         self.db.add(room_member)
