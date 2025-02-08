4346889e7b38490ecfcf48bb8060d88847d126a3
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-08 22:44:56 +0800

    fix: ruff linter error

--- src/utils/token_helper.py
+++ src/utils/token_helper.py
@@ -20,9 +20,9 @@ def auth_required(func: Callable) -> Callable:
     """
 
     def wrapper(
-        self,
+        self,  # noqa: ANN001
         request: grpc.aio.Call,
-        context: grpc.aio.ServicerContext,  # noqa: ANN001
+        context: grpc.aio.ServicerContext,
     ) -> grpc.aio.Call:
         metadata = dict(context.invocation_metadata())
         token = metadata.get("authorization")
