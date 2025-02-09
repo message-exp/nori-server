7f9ed43008659d7cb82b496cad73308315ef7f1f
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-09 17:04:08 +0800

    fix: get_user return type
    Format the file by the way.

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -7,14 +7,18 @@ class UserRepository:
     def __init__(self, session: Session) -> None:
         self.db = session
 
-    def get_user(self, user_id: Optional[int] = None, email: Optional[str] = None) -> Users:
+    def get_user(
+        self, user_id: Optional[int] = None, email: Optional[str] = None
+    ) -> Users | None:
         if user_id is not None:
             return self.db.exec(select(Users).where(Users.id == user_id)).first()
         if email is not None:
             return self.db.exec(select(Users).where(Users.email == email)).first()
         return None
 
-    def exists_user(self, user_id: Optional[int] = None, email: Optional[str] = None) -> bool:
+    def exists_user(
+        self, user_id: Optional[int] = None, email: Optional[str] = None
+    ) -> bool:
         if user_id is not None:
             user = self.db.exec(select(Users).where(Users.id == user_id)).first()
         elif email is not None:
