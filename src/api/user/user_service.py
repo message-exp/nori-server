b04e8e9305e3fe5197f60c5133de2c7b6bdac4e9
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-09 22:30:23 +0800

    feat: MES-46 get user endpoint implementation (#14)
    * chore: update protos to v0.2
    
    * feat: implement GetUser method
    
    * perf: reduce query overhead
    
    Avoid to load the unnecessary `Rooms` non-id columns in Users.rooms
    
    * fix: ruff linter error
    
    * fix: user repository get_user return type
    
    * test: GetUser method tests
    
    * fix: ruff format error
    
    * refactor: rename test to tests
    
    * refactor: rename get_user function
    
    * fix: test error
    
    * refactor: change repo name
    
    * fix: test bypass auth
    
    * refactor(test): simplize the auth issue
    
    * refactor: rename test file
    
    * fix: ruff lint error
    
    * refactor: remove test class

--- src/api/user/user_service.py
+++ src/api/user/user_service.py
@@ -1,3 +1,4 @@
+import grpc
 from grpc.aio import ServicerContext
 
 from src.utils.token_helper import auth_required
@@ -7,11 +8,16 @@ from proto_generated.nori.v0.user.user_pb2 import User
 from proto_generated.nori.v0.user.user_login_pb2 import UserEmailPasswordLogin
 from proto_generated.nori.v0.user.user_id_pb2 import UserId
 from proto_generated.nori.v0.user.user_profile_pb2 import UserProfile
+from proto_generated.nori.v0.user.user_connection_pb2 import UserConnection
+from proto_generated.nori.v0.room.room_id_pb2 import RoomId
 from proto_generated.nori.v0.room.room_list_pb2 import RoomList
 
 from proto_generated.nori.v0.user.user_service_pb2_grpc import (
     UserServiceServicer,
 )
+
+from utils.db_helper import get_db
+from repositories import UserRepo
 
 
 class UserServicer(UserServiceServicer):
@@ -25,10 +31,29 @@ class UserServicer(UserServiceServicer):
 
     @auth_required
     def GetUser(self, request: UserId, context: ServicerContext) -> User:
-        # TODO: check user_id format
-        # TODO: get user data from database
-        # TODO: return user data
-        return User()
+        # get user ID from request
+        user_id = request.id
+
+        # get user data from database
+        user_repo = UserRepo(next(get_db()))
+        user = user_repo.get_user_only_ids(user_id)
+
+        # check if user exists in database
+        if user is None:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"User with ID {user_id} not found.")
+            return User()
+
+        # return user data
+        return User(
+            user_id=UserId(id=user.id),
+            username=str(user.username),
+            email=str(user.email),
+            display_name=str(user.display_name),
+            avatar_url="",
+            connected_accounts=[UserConnection()],
+            rooms=[RoomId(id=room.id) for room in user.rooms],
+        )
 
     def Login(self, request: UserEmailPasswordLogin, context: ServicerContext) -> Empty:
         # TODO: check email format
