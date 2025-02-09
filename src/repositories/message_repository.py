93046582cd8238a78e826eb3d74625719cee6d1f
barney9211 <barney9211@gmail.com>
2025-02-09 14:01:17 +0000

    Modify:change getMessage logic

--- src/repositories/message_repository.py
+++ src/repositories/message_repository.py
@@ -1,10 +1,19 @@
-from sqlmodel import Session ,select
+from datetime import datetime
+from sqlmodel import Session ,select 
 from model import Messages ,Rooms ,RoomMembers
 
 class MessageRepository:
     def __init__(self, session: Session) -> None:
         self.db = session
-    def get_message_by_roomId(self ,room_id : int) -> Messages:
-        return self.db.exec(select(Messages).where(Messages.room_id == room_id)).one_or_none()
+    def get_message_by_roomId(self ,room_id : int , return_quantity : int , from_when : datetime , lim :int) -> list:
+        return self.db.exec(select(Messages).where(Messages.room_id == room_id).where(Messages.created_at<from_when).limit(lim)).all()
+    def add_message_to_db(self , message : Messages) -> int:
+        self.db.add(message)
+        self.db.commit()
+        return message.id
+    def update_message_to_db(self , room_id :int , id : int) -> None:
+        #Update
+        return None
+        
 
 
