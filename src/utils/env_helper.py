f2e17119bb32a486159e1a0abf0b2da84895adfb
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-09 21:32:20 +0800

    feat: create env helper utility

---
+++ src/utils/env_helper.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
