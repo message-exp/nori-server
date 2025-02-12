dc295076036ea54592278679b9add84c0038fa8b
barney9211 <barney9211@gmail.com>
2025-02-12 09:04:08 +0000

    Fix: fix the bug in message_service and message_repository

--- src/api/message/message_service.py
+++ src/api/message/message_service.py
@@ -1,18 +1,16 @@
-from grpc.aio import ServicerContext
 import grpc
-from google.protobuf.empty_pb2 import Empty
+from grpc.aio import ServicerContext
+
 from proto_generated.nori.v0.message.message_pb2 import Message
-from proto_generated.nori.v0.room.room_id_pb2 import RoomId
 from proto_generated.nori.v0.message.message_service_pb2_grpc import (
     MessageServiceServicer,
 )
 from proto_generated.nori.v0.message.message_id_pb2 import MessageId
-from src.model import Messages
+from model import Messages
 from src.proto_generated.nori.v0.message.get_message_request_pb2 import GetMessageRequest
 from utils.db_helper import get_db
 from src.utils.token_helper import auth_required
-
-from repositories import UserRepo, MessageRepo, RoomRepo
+from repositories import UserRepo, MessageRepo , RoomRepo
 
 
 class MessageServicer(MessageServiceServicer):
@@ -20,42 +18,40 @@ class MessageServicer(MessageServiceServicer):
     auth_config: dict[str, bool] = dict()
 
     @auth_required
-    def SendMessage(self, request: Message, context: ServicerContext) -> MessageId:
-        # TODO: ...... (implement send message logic)
-        # context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        # context.set_details('Method not implemented!')
-        user_id = request.author
-        room_id = request.room_id
+    def SendMessage(self, request: Message, context: ServicerContext) -> MessageId | None:
+        user_id = request.author.id
+        room_id = request.room_id.id
         message = request.text
         userDB = UserRepo(next(get_db()))
-        user_exist = userDB.exists_user(user_id=user_id)
+        user = userDB.get_user(user_id=user_id)
         # check user exist
-        if not user_exist:
+        if user is None:
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details(f"User with ID {user_id} not found.")
-        # check room exist
+            return None
         roomDB = RoomRepo(next(get_db()))
         room_exist = roomDB.exists_room(room_id=room_id)
+        # check room exist
         if not room_exist:
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details(f"Room with ID {room_id} not found.")
+            return None
         messageDB = MessageRepo(next(get_db()))
-        user = userDB.get_user(user_id=user_id)
-
         input_message_id = messageDB.add_message_to_db(
             Messages(
+
                 room_id=room_id,
                 message=message,
                 user=user
             )
-        )
+        ).id
         return MessageId(id=input_message_id)
 
-    @auth_required
-    def GetMessages(self, request: GetMessageRequest, context: ServicerContext) -> Message:
-        # TODO: ...... (implement get messages logic)
-        baseline = request.baseline
-        limit = request.limit
-        roomId: int = request.room_id
-        message_db = MessageRepo(next(get_db()))
-        return message_db.get_message_by_roomId(roomId)
+    # @auth_required
+    # def GetMessages(self, request: GetMessageRequest, context: ServicerContext) -> Message:
+    #     # TODO: ...... (implement get messages logic)
+    #     baseline = request.baseline
+    #     limit = request.limit
+    #     roomId: int = request.room_id
+    #     message_db = MessageRepo(next(get_db()))
+    #     return message_db.get_message_by_roomId(roomId)
--- src/repositories/message_repository.py
+++ src/repositories/message_repository.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from sqlmodel import Session ,select 
-from model import Messages ,Rooms ,RoomMembers
+from model import Messages
 
 class MessageRepository:
     def __init__(self, session: Session) -> None:
         self.db = session
-    def get_message_by_roomId(self ,room_id : int , return_quantity : int , from_when : datetime , lim :int) -> list:
-        return self.db.exec(select(Messages).where(Messages.room_id == room_id).where(Messages.created_at<from_when).limit(lim)).all()
-    def add_message_to_db(self , message : Messages) -> int:
+    def get_message_by_roomId(self ,room_id : int , return_quantity : int , from_when : datetime , lim :int) -> list[Messages]:
+        return list(self.db.exec(select(Messages).where(Messages.room_id == room_id).where(Messages.created_at<from_when).limit(lim)).all())
+    def add_message_to_db(self , message : Messages) -> Messages:
         self.db.add(message)
         self.db.commit()
-        return message.id
+        return message
     def update_message_to_db(self , room_id :int , id : int) -> None:
         #Update
         return None
