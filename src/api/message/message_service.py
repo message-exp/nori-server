e8c4a869c8002d64d4205c9db5d3453c37c5bd29
barney9211 <barney9211@gmail.com>
2025-02-20 14:03:05 +0000

    Modify:add type to varible

--- src/api/message/message_service.py
+++ src/api/message/message_service.py
@@ -49,11 +49,11 @@ class MessageServicer(MessageServiceServicer):
     def GetMessages(
         self, request: GetMessageRequest, context: ServicerContext
     ) -> list[Messages]:
-        baseline = request.baseline.id
-        limit = request.limit
-        room_id: int = request.room_id.id
+        baseline : int = request.baseline.id
+        limit : int= request.limit
+        room_id : int = request.room_id.id
         room_repo = RoomRepo(next(get_db()))
-        room_exist = room_repo.exists_room(room_id=room_id)
+        room_exist : bool = room_repo.exists_room(room_id=room_id)
         if not room_exist:
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details(f"Room with ID {room_id} not found.")
