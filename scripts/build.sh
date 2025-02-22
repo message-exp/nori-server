a378bcf3155d99cc0d2d30bb0e95cdbd7a907132
ErEr <jeffliao92316046@gmail.com>
2025-02-22 20:32:29 +0800

    Chore: update wording

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,7 @@ cd -
 echo ">>> 啟動 Docker Compose"
 docker compose up -d
 
-echo ">>> 進入 Docker 容器並執行指令"
+echo ">>> 執行 alembic migrate"
 docker exec -it nori-server-app-1 /bin/sh -c "cd src && poetry run alembic upgrade head"
 
 
