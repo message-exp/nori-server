689e951203a9951edfb88421145f193f4c8a776b
ErEr <jeffliao92316046@gmail.com>
2025-03-01 10:17:10 +0000

    Feat: add Kafka configuration to environment variables

--- src/utils/config.py
+++ src/utils/config.py
@@ -13,6 +13,8 @@ class Config:
 
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "jwt_default_secret")
     JWT_ALGORITHM: str = "HS512"
-
+    KAFKA_HOST: str = os.getenv("KAFKA_HOST", "localhost")
+    KAFKA_PORT: str = os.getenv("KAFKA_PORT", "9092")
+    KAFKA_SERVER:str = f"{KAFKA_HOST}:{KAFKA_PORT}"
 
 config = Config()
