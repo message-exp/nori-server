0f0a6ac4176035fd72b6174b3bffcafd1eeb4ba3
root <barney9211@gmail.com>
2025-03-13 14:50:21 +0000

    Feat:update_refresh_token

--- src/repositories/refresh_token_repository.py
+++ src/repositories/refresh_token_repository.py
@@ -8,10 +8,11 @@ class RefreshTokenRepository:
 
     def exists_refresh_token(
         self,
+        user_id: int,
         refresh_token: str,
     ) -> bool:
         refresh_token_found = self.db.exec(
-            select(RefreshToken).where(RefreshToken.refresh_token == refresh_token)
+            select(RefreshToken).where(RefreshToken.refresh_token == refresh_token and RefreshToken.user_id == user_id)
         ).first()
         return refresh_token_found is not None
 
@@ -19,3 +20,11 @@ class RefreshTokenRepository:
         self.db.add(refresh_token)
         self.db.commit()
         return refresh_token.id
+    def update_refresh_token(self,user_id : int, refresh_token: RefreshToken) -> None:
+        refresh_token_found = self.db.exec(
+            select(RefreshToken).where(RefreshToken.user_id == user_id & RefreshToken.refresh_token == refresh_token)
+        ).first()
+        if refresh_token_found:
+           refresh_token_found.refresh_token = refresh_token.refresh_token
+           self.db.add(refresh_token_found)
+           self.db.commit()
