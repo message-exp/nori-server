4a05e8fcc73fde21ae08264c9306718d46e36ae0
root <barney9211@gmail.com>
2025-03-14 02:20:49 +0000

    Refactor:remove auth_required and feat New RefreshToken return

--- src/repositories/refresh_token_repository.py
+++ src/repositories/refresh_token_repository.py
@@ -20,11 +20,12 @@ class RefreshTokenRepository:
         self.db.add(refresh_token)
         self.db.commit()
         return refresh_token.id
-    def update_refresh_token(self,user_id : int, refresh_token: RefreshToken) -> None:
+    def update_refresh_token(self,user_id : int, old_refresh_token: str , new_refresh_token : str) -> None:
+        # use old refresh token to find the RefreshToken in the database
         refresh_token_found = self.db.exec(
-            select(RefreshToken).where(RefreshToken.user_id == user_id & RefreshToken.refresh_token == refresh_token)
+            select(RefreshToken).where(RefreshToken.user_id == user_id & RefreshToken.refresh_token == old_refresh_token)
         ).first()
         if refresh_token_found:
-           refresh_token_found.refresh_token = refresh_token.refresh_token
+           refresh_token_found.refresh_token = new_refresh_token
            self.db.add(refresh_token_found)
            self.db.commit()
